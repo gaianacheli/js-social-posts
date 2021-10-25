ca6b297b1e4441a44bfa054b200399964d30793f
gaianacheli <gaianacheli04@gmail.com>
2021-10-25 15:28:41 +0200

    creato array e creazione struttura post

---
+++ asset/js/main.js
@@ -0,0 +1,29 @@
+const post = [
+{
+    id:'Francesco',
+    avatar: 'https://i.picsum.photos/id/315/300/300.jpg?hmac=YWqnPrHCKQAIUAZSbXNjekfq1TbcljqwqzTNfoBQVHE',
+    data: 20/09/2021,
+    contenuto: ' Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    image: 'https://i.picsum.photos/id/315/300/300.jpg?hmac=YWqnPrHCKQAIUAZSbXNjekfq1TbcljqwqzTNfoBQVHE',
+    likes: 10
+},
+{
+    
+    id: 'Sara',
+    avatar:'https://i.picsum.photos/id/563/300/300.jpg?hmac=_yODwhC9wmOj8yM7resDVXCVzX1HrmDC5D5dakomZPE',
+    data: 8/10/2021,
+    contenuto: ' Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    image: 'https://i.picsum.photos/id/563/300/300.jpg?hmac=_yODwhC9wmOj8yM7resDVXCVzX1HrmDC5D5dakomZPE',
+    likes: 3
+},
+{
+    
+    id: 'Manuel',
+    avatar: 'https://i.picsum.photos/id/26/300/300.jpg?hmac=UMOujtfr34V3cEr43DgbCJYhdXQl8hbrc-GxqQd_Fug',
+    data: 24/10/2021,
+    contenuto: ' Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    image: 'https://i.picsum.photos/id/26/300/300.jpg?hmac=UMOujtfr34V3cEr43DgbCJYhdXQl8hbrc-GxqQd_Fug',
+    likes: 100
+}
+]
+const like = ['Francesco','Sara','Manuel']
